e1fa2c7a604174eab4fdbfbbd09f5873a682e7c7
Frederic M <frederic.monjo@me.com>
2017-01-25 22:58:32 +0100

    MongoDb simple test on QueryVerticle
    Use a simple MongoDB database connection from the QueryVerticle.
    Everything hardcoded for now as this is a minimalistic experiment.

--- scales-query/src/main/java/studio/lysid/scales/query/QueryVerticle.java
+++ scales-query/src/main/java/studio/lysid/scales/query/QueryVerticle.java
@@ -2,8 +2,10 @@ package studio.lysid.scales.query;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.mongo.MongoClient;
 import studio.lysid.scales.deploy.EventBusServiceHelper;
 import studio.lysid.scales.deploy.Service;
 import studio.lysid.scales.query.scale.QueryScaleService;
@@ -13,10 +15,20 @@ public class QueryVerticle extends AbstractVerticle {
 
     private final Logger logger = LoggerFactory.getLogger(QueryVerticle.class);
 
+    private MongoClient db;
+
     @Override
     public void start(Future<Void> startFuture) throws Exception {
+
+        JsonObject dbConfig = new JsonObject()
+                .put("connection_string", "mongodb://localhost:27017")
+                .put("db_name","scales-query");
+        this.db = MongoClient.createShared(this.vertx, dbConfig);
+
         new EventBusServiceHelper(this.vertx, logger)
-                .publish(QueryScaleService.class, new QueryScaleServiceImpl(), Service.QueryScale.address);
+                .publish(QueryScaleService.class, new QueryScaleServiceImpl(this.db), Service.QueryScale.address);
+
+
         startFuture.complete();
     }
 
--- scales-query/src/main/java/studio/lysid/scales/query/scale/QueryScaleServiceImpl.java
+++ scales-query/src/main/java/studio/lysid/scales/query/scale/QueryScaleServiceImpl.java
@@ -6,20 +6,31 @@ import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.mongo.MongoClient;
 
 public class QueryScaleServiceImpl implements QueryScaleService {
 
     private static final Logger logger = LoggerFactory.getLogger(QueryScaleServiceImpl.class);
+    private static final String ScaleCollection = "scale";
+
+    private final MongoClient db;
+
+    public QueryScaleServiceImpl(MongoClient db) {
+        super();
+        this.db = db;
+    }
 
     @Override
     public void findScaleById(int id, Handler<AsyncResult<JsonObject>> handler) {
         logger.debug("findScaleById called with parameters: id={0}", id);
-        if (id > 0) {
-            handler.handle(Future.succeededFuture(
-                    new JsonObject().put("id", id).put("title", "This is the scale #" + id))
-            );
-        } else {
-            handler.handle(Future.failedFuture("Scale #" + id + " does not exist"));
-        }
+
+        db.findOne(ScaleCollection, new JsonObject().put("_id", Integer.toString(id)), new JsonObject(),
+            res -> {
+                if (res.succeeded() && res.result() != null) {
+                    handler.handle(Future.succeededFuture(res.result()));
+                } else {
+                    handler.handle(Future.failedFuture("Scale #" + id + " does not exist"));
+                }
+            });
     }
 }
