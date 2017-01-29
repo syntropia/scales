b65c25436e964146c8e12680fe2881398e75b203
Frederic M <frederic.monjo@me.com>
2017-01-30 00:45:06 +0100

    Tranformed Facade tests into Scales integration test
    But tests are not run by maven, most probably due to the fat-jar
    packaging. I’ll check that later since for now quick manual testing on
    deployed app is enough.

--- scales-launcher/src/test/java/ScalesIntegrationTests.java
+++ scales-launcher/src/test/java/ScalesIntegrationTests.java
@@ -1,56 +1,49 @@
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import studio.lysid.scales.Defaults;
-import studio.lysid.scales.facade.FacadeVerticle;
-import studio.lysid.scales.query.QueryVerticle;
+import studio.lysid.scales.deploy.LauncherVerticle;
+import studio.lysid.scales.deploy.Verticle;
 
 @RunWith(VertxUnitRunner.class)
-public class FacadeVerticleTest {
+public class ScalesIntegrationTests {
 
     private Vertx vertx;
 
-    @Before
-    public void setUp(TestContext context) {
+    @BeforeClass
+    public void verticlesSetUp(TestContext context) {
         this.vertx = Vertx.vertx();
-        this.vertx.deployVerticle(QueryVerticle.class.getName(), qvar -> {
-            if (qvar.succeeded()) {
-                DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port", Defaults.FacadeHttpPort));
-                this.vertx.deployVerticle(FacadeVerticle.class.getName(), options, fvar -> {
-                    if (fvar.succeeded()) {
-                        context.async().complete();
-                    } else {
-                        context.fail(fvar.cause());
-                    }
-                });
+        this.vertx.deployVerticle(LauncherVerticle.class.getName(), ar -> {
+            if (ar.succeeded()) {
+                context.async().complete();
             } else {
-                context.fail(qvar.cause());
+                context.fail(ar.cause());
             }
         });
     }
 
-    @After
-    public void tearDown(TestContext context) {
+    @AfterClass
+    public void verticlesTearDown(TestContext context) {
         vertx.close(context.asyncAssertSuccess());
     }
+
+
 
     @Test
     public void testSampleService(TestContext context) {
 
         final Async async = context.async();
         HttpClient client = vertx.createHttpClient();
+        int deployedHttpPort = Verticle.Facade.deploymentOptions.getConfig().getInteger("http-port");
 
         // Any id should be accepted in this first test implementation
         client.getNow(
-                Defaults.FacadeHttpPort, "localhost", "/scale/13",
+                deployedHttpPort, "localhost", "/scale/13",
                 response -> {
                     response.handler(body -> {
                         context.assertTrue(body.toString().contains("#13"));
@@ -59,7 +52,7 @@ public class FacadeVerticleTest {
 
         // Negative ids are incorrect, they should be rejected
         client.getNow(
-                Defaults.FacadeHttpPort, "localhost", "/scale/-13",
+                deployedHttpPort, "localhost", "/scale/-13",
                 response -> {
                     response.handler(body -> {
                         context.assertTrue(body.toString().contains("does not exist"));
